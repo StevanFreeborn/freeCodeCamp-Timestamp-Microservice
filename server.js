8405318d38e2de5fc31b1fce3147e9f8cb9d901f
StevanFreeborn <sfreeborn7@gmail.com>
2022-04-27 17:10:36 -0500

    implement date route

--- server.js
+++ server.js
@@ -14,11 +14,55 @@ app.get("/", function (req, res) {
 });
 
 // initial test route
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/greeting", (req, res) => {
+  res.json({greeting: 'hello world'});
+});
+
+// date route
+app.get("/api/:date?", (req, res) => {
+
+  let dateString = req.params.date
+
+  // no date provided
+  if (dateString == undefined) {
+
+    res.json({
+      unix: new Date().getTime(),
+      utc: new Date().toUTCString()
+    });
+
+    return;
+  }
+
+  // attempt to parse string param value to date
+  date = new Date(dateString);
+
+  // if parsing param value as string fails
+  // try parsing param value to number then to date
+  if (date == 'Invalid Date') {
+    date = new Date(parseInt(dateString));
+  }
+
+  // if cannot parse param value as string or number to date
+  // respond with error message.
+  if (date == 'Invalid Date') {
+    res.json({
+      error: 'Invalid Date'
+    });
+
+    return;
+  }
+
+  // otherwise respond with object that contains
+  // date in milliseconds and a utc string
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  })
+
 });
 
 // listen for requests
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
