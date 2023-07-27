d08c62d06ee0d3f3b2c1ec34fcec6704759a0dfc
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-07-27 10:46:59 -0500

    fix: dynamically set urls on index view based on current windows href

--- public/index.js
+++ public/index.js
@@ -1,7 +1,12 @@
-document.getElementById('form').addEventListener("submit", function (e) {
-    e.preventDefault()
+document.getElementById('form').addEventListener('submit', function (e) {
+  e.preventDefault();
 
-    let param = document.querySelector('input[name="date"]').value;
-    console.log(param);
-    window.location.href = 'api/' + param;
-})
+  let param = document.querySelector('input[name="date"]').value;
+  console.log(param);
+  window.location.href = 'api/' + param;
+});
+
+document.getElementById('baseUrl').innerText = window.location.href;
+
+document.getElementById('exampleRequestUrl').innerText =
+  window.location.href + 'api/2015-12-25';
